423a5e3860414991cdd06d623cd627d887b5c6e5
JANARTHANAN <102517166+JanaPalani@users.noreply.github.com>
2022-12-05 16:52:37 +0530

    Add files via upload

---
+++ day5/task2.py
@@ -0,0 +1,16 @@
+data = [[],['S','M','R','N','W','J','V','T'],['B','W','D','J','Q','P','C','V'],
+        ['B','J','F','H','D','R','P'],['F','R','P','B','M','N','D'],['H', 'V', 'R', 'P', 'T', 'B'],['C', 'B', 'P', 'T'],['B', 'J', 'R', 'P', 'L'],['N', 'C', 'S', 'L', 'T', 'Z', 'B', 'W'],['L','S','G']]
+
+my_file = open('data.txt','r')
+data_move = my_file.read()
+data_move=data_move.split('\n')
+for i in range(len(data_move)):
+    data_move[i] = data_move[i].split(' ')
+    data_move[i].remove('move')
+    data_move[i].remove('from')
+    data_move[i].remove('to')
+    data_move[i] = list(map(int,data_move[i]))
+print(data_move)
+for i in data_move:
+    data[i[2]] = data[i[2]]+data[i[1]][-i[0]:]
+
